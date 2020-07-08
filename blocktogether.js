20e2699a1ce475e18672b15f640bfb4439e4fe45
John Visser <jvisser@carterra-bio.com>
2020-07-08 12:05:39 -1000

    Revert "Disable signups."
    This reverts commit 2e25b3b4b68ab52c03a534e80dfd417a92cc0031.

--- blocktogether.js
+++ blocktogether.js
@@ -129,8 +129,20 @@ function passportSuccessCallback(accessToken, accessTokenSecret, profile, done) 
       }
     }).then(function(btUser) {
       if (!btUser) {
-        done(null, undefined);
-        return;
+        return BtUser.create({
+          uid: uid,
+          screen_name: screen_name,
+          access_token: accessToken,
+          access_token_secret: accessTokenSecret,
+          shared_blocks_key: null,
+          block_new_accounts: false,
+          block_low_followers: false,
+          follow_blocktogether: false,
+          pendingActions: 0,
+          paused: false,
+          blockCount: null,
+          deactivatedAt: null
+        });
       } else {
         // The user's access token may have changed since last login, or they may
         // have been previously deactivated. Overwrite appropriate values with
